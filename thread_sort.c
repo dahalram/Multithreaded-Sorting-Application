0659ad89afec07b54a6d9a514cc4a0d7d31564aa
Ram <ramhari.dahal1@gmail.com>
2018-02-16 06:21:22 -0700

    Merge and read file

--- thread_sort.c
+++ thread_sort.c
@@ -1,25 +1,55 @@
-#include <iostream>
 #include <stdlib.h>
 #include <pthread.h>
 #include <stdio.h>
+#include <string.h>
 
 typedef struct {
 	int low;
 	int high;
 } Array;
 
-void merge(int a, int b) {
+void merge(int arr[], int left, int right, int mid) {
 
 }
 
-void *mergesort(void *arr) {
-
+void mergesort(int arr[], int left, int right) {
+	if (left < right) {
+		int mid = (left + (right -1))/2;
+		mergesort(arr, left, mid);
+		mergesort(arr, mid+1, right);
+	}
 }
 
-int main() {
+int *readfile(char *filename) {
+	FILE *file;
+	file = fopen(filename, "rt");
+	char line[80];
+	int len = 0;
+	while (fgets(line, 80, file) != NULL) {
+		len += 1;
+	}
+	
+	int i = 0, j;
+	int *inp = malloc(len*sizeof(int));
+
+	while (fgets(line, 80, file) != NULL) {
+		sscanf(line, "%d", &j);
+		inp[i] = j;
+		i++;
+	}
+	fclose(file);
+
+	return inp;
+	
+}
+
+int main(int argc, char *argv[]) {
 	// Create threads
 	FILE *file;
 	file = fopen("input.txt", "r");
+
+	char *f = argv[1];
+	int *arr = readfile(f);
 
 	pthread_t thread;
 	pthread_create(&thread, NULL, mergesort, &arr);
